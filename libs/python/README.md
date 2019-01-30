87a1d306273cc1712c188c9f6afe6b5a173fe8d0
GPlay97 <GPlay97@users.noreply.github.com>
2019-01-30 22:51:39 +0100

    first implementation of python lirbary for v2 api

---
+++ libs/python/README.md
@@ -0,0 +1,17 @@
+## EVNotify Python Library
+
+### Prerequsites
+- Python 3
+
+### Dependencies
+- requests
+
+#### Installation
+
+##### First-Time-Setup
+- Install Python 3
+- Install required dependency (`pip install requests`)
+
+##### Usage
+- Import the EVNotify Python library file (`import evnotify`)
+- Refer to the API documentation on how to use the library
---
+++ libs/python/evnotify.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+class EVNotify:
+    
+    def __init__(self, akey = None, token = None):
+        self.RESTURL = 'https://app.evnotify.de/'
+        self.akey = akey
+        self.token = token
+
+    def sendRequest(self, method, fnc, useAuthentication = False, params = {}):
+        return getattr(requests, method)(self.RESTURL + fnc, json=params).json()
+
+    def getKey(self):
+        return self.sendRequest('get', 'key')['akey']
+
+    def register(self, akey, password):
+        return self.sendRequest('post', 'register', False, {
+            "akey": akey,
+            "password": password
+        })
